de98f4bebd7306b64dfa35d919aaea28910c7e31
DANIAL AHMAD <109754828+danialahmad5557@users.noreply.github.com>
2024-03-23 22:19:18 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
 let interval;
 let time = { minutes: 0, seconds: 0, milliseconds: 0 };
+let startButton = document.querySelector('.start');
 
 function start() {
-	interval = setInterval(() => {
-		time.milliseconds += 10;
-		if (time.milliseconds >= 1000) {
-			time.milliseconds = 0;
-			time.seconds++;
-			if (time.seconds >= 60) {
-				time.seconds = 0;
-				time.minutes++;
-			}
-		}
-		document.querySelector('.minutes').innerText = format(time.minutes);
-		document.querySelector('.seconds').innerText = format(time.seconds);
-		document.querySelector('.milliseconds').innerText = time.milliseconds;
-	}, 10);
+  if (startButton.disabled) return;
+  startButton.disabled = true;
+  interval = setInterval(() => {
+    time.milliseconds += 10;
+    if (time.milliseconds >= 1000) {
+      time.milliseconds = 0;
+      time.seconds++;
+      if (time.seconds >= 60) {
+        time.seconds = 0;
+        time.minutes++;
+      }
+    }
+    document.querySelector('.minutes').innerText = format(time.minutes);
+    document.querySelector('.seconds').innerText = format(time.seconds);
+    document.querySelector('.milliseconds').innerText = time.milliseconds;
+  }, 10);
 }
 
 function pause() {
-	clearInterval(interval);
+  clearInterval(interval);
+  startButton.disabled = false;
 }
 
 function reset() {
-	clearInterval(interval);
-	time = { minutes: 0, seconds: 0, milliseconds: 0 };
-	document.querySelector('.minutes').innerText = '00';
-	document.querySelector('.seconds').innerText = '00';
-	document.querySelector('.milliseconds').innerText = '000';
+  clearInterval(interval);
+  time = { minutes: 0, seconds: 0, milliseconds: 0 };
+  document.querySelector('.minutes').innerText = '00';
+  document.querySelector('.seconds').innerText = '00';
+  document.querySelector('.milliseconds').innerText = '000';
+  startButton.disabled = false;
 }
 
 function format(num) {
-	return num < 10 ? '0' + num : num;
+  return num < 10 ? '0' + num : num;
 }
